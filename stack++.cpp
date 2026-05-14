967ba5d85b3a10ca13189b13807a1d505afc48ce
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-14 10:33:52 +0700

    menambahkan class stack

--- stack++.cpp
+++ stack++.cpp
@@ -13,3 +13,27 @@ public:
         next = NULL;
     }
 };
+
+// stack class
+class stack
+{
+private:
+    Node *top;
+
+public:
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    
+}
