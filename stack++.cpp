2911f1c537c2c4c2f8a247a3b3ea13efefb9f7f1
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-14 10:50:49 +0700

    menambahkan penyempurnaan fungsi display dengan output

--- stack++.cpp
+++ stack++.cpp
@@ -57,8 +57,12 @@ public:
         {
             Node *current = top;
             while (current != NULL)
-        }
-        
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }   
     }
 
 
